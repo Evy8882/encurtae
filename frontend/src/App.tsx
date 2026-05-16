dceb68825b592b4d3c419c278cca91f9541765b3
Evy8882 <evy1339@gmail.com>
2026-05-16 18:48:06 -0300

    Adiciona estilização a tela inicial do app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,8 +6,12 @@ function App() {
     <>
       <main>
         <h1>Encurtaê</h1>
-        <p>Crie links encurtados para suas URLs</p>
+        <p>Crie links encurtados para suas URL's</p>
         <section>
+          <div className='new-url-container'>
+            <input type="text" placeholder="Cole sua URL aqui..." />
+            <button>Adicionar</button>
+          </div>
           <h2>Suas URLS:</h2>
           <ul></ul>
         </section>

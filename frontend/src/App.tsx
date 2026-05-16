fb60d83d5795a136da37f205918bba567c5fa373
Evy8882 <evy1339@gmail.com>
2026-05-16 18:23:50 -0300

    Adiciona estrutura inicial do frontend com react

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <main>
+        <h1>Encurtaê</h1>
+        <p>Crie links encurtados para suas URLs</p>
+        <section>
+          <h2>Suas URLS:</h2>
+          <ul></ul>
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App
